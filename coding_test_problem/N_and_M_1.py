bebdde98a9e02e40841ba75b51a3da739d355e05
Won-young <dnjsdud2257@gmail.com>
2021-08-12 20:19:58 +0900

    코딩테스트 N 과 M(백트랙킹 풀이 완료)

---
+++ coding_test_problem/N_and_M_1.py
@@ -0,0 +1,23 @@
+# 수열
+#입력 N: M:
+#1부터 N까지 자연수 중 중복 없이 M개를 고른 수열
+
+N, M = map(int, input().split(" "))
+visited = [False] *N
+out = []
+
+def solve(depth, N, M):
+    if depth ==M:
+        # 출력
+        print(" ".join(map(str, out)))
+        return
+
+    for i in range(len(visited)):
+        if not visited[i]:
+            visited[i] =True
+            out.append(i+1)
+            solve(depth+1, N,M)
+            visited[i] = False
+            out.pop()
+
+solve(0, N, M)
